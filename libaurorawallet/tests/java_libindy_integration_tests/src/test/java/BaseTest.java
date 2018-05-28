695158bbbd15cc6650adb912e54fca7967804a4e
Nemanja Veskovic <nemanja.veskovic@gmail.com>
2018-05-28 18:04:31 +0200

    java libindy integration tests

---
+++ libaurorawallet/tests/java_libindy_integration_tests/src/test/java/BaseTest.java
@@ -0,0 +1,18 @@
+public class BaseTest {
+
+    protected static final String POOL = "Pool1";
+
+    protected static final String CONFIG = "{" +
+            "   \"read_host\": \"localhost\"," +
+            "   \"write_host\": \"localhost\"," +
+            "   \"port\": 3306" +
+            "}";
+    protected static final String CREDENTIALS = "{" +
+            "    \"key\": \"MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=\"," +
+            "    \"storage_credentials\": {" +
+            "        \"user\": \"wallet\"," +
+            "        \"pass\": \"wallet\"" +
+            "    }" +
+            "}";
+
+}
