1f6cfc5012f40675cc0c4c7b58e775f8315f3747
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2020-05-22 19:48:38 +0300

    test
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- devops/set_version.sh
+++ devops/set_version.sh
@@ -4,6 +4,10 @@ PACKAGE_VERSION=$1
 SUFFIX=$2
 BRANCH=$3
 
+echo "Version: "${PACKAGE_VERSION}
+echo "Suffix: "${SUFFIX}
+echo "Branch: "${BRANCH}
+
 VERSION=PACKAGE_VERSION
 
 if [[ BRANCH != "stable" ]]
@@ -13,5 +17,9 @@ fi
 
 echo "Version: "${VERSION}
 
-
 fpm -s deb -t deb --version ${VERSION} libmysqlstorage_${PACKAGE_VERSION}_amd64.deb
+
+if [[ BRANCH != "stable" ]]
+then
+	rm libmysqlstorage_${PACKAGE_VERSION}_amd64.deb
+fi
