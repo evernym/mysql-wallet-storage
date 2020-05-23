a54cb5ce36b21c64756b67945a3e2b41a64db64a
Nikita Khateev <hateevnikita@yandex.ru>
2020-05-23 23:55:39 +0300

    Fix length of type field -- it can be bigger then 64 symbols

--- db_scripts/schema/change_scripts/wallet_schema_creation.2018-05-07.sql
+++ db_scripts/schema/change_scripts/wallet_schema_creation.2018-05-07.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `wallets` (
 CREATE TABLE IF NOT EXISTS `items` (
     `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     `wallet_id` BIGINT(20) NOT NULL,
-    `type` VARCHAR(64) NOT NULL,
+    `type` VARCHAR(128) NOT NULL,
     `name` VARCHAR(1024) NOT NULL,
     `value` LONGBLOB NOT NULL,
     `tags` JSON NOT NULL,
